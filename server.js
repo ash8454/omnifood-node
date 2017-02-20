a1f3d09795820b4e53cf2b8bbca07a36b3fcf055
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-02-20 15:12:28 -0500

    Updated code

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('omnifood'));
+app.use(express.static(__dirname + '/omnifood'));
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
