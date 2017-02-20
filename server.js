66b69b1b64849771912d09692db54f0e9e81c5ba
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-02-20 15:41:08 -0500

    Changed directory navigation

--- server.js
+++ server.js
@@ -3,9 +3,6 @@ const express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
 console.log("Process PORT is: " + process.env.PORT);
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/omnifood'));
@@ -14,7 +11,7 @@ app.use(express.static(__dirname + '/omnifood'));
 app.get('/', function(req, res) {
 
 	// ejs render automatically looks in the views folder
-	res.render('index');
+	res.render('index.html');
 });
 
 app.listen(port, function() {
