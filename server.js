004083478c3e4264e3b059afb905dca98414b3a0
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-02-20 15:50:49 -0500

    Removed not working code

--- server.js
+++ server.js
@@ -3,13 +3,7 @@ const express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
-console.log("Process PORT is: " + process.env.PORT);
 
-var engines = require('consolidate');
-
-app.set('omnifood', __dirname + '/omnifood');
-app.engine('html', engines.mustache);
-app.set('view engine', 'html');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/omnifood'));
@@ -22,5 +16,6 @@ app.get('/', function(req, res) {
 });
 
 app.listen(port, function() {
+	console.log("Process PORT is: " + process.env.PORT);
 	console.log('Our app is running on http://localhost:' + port);
 });
