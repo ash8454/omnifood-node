e1004ddd9c52b23ad562d010440a5df94df5693a
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-02-20 15:46:39 -0500

    Added consolidate library

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ var app = express();
 const port = process.env.PORT || 3000;
 
 console.log("Process PORT is: " + process.env.PORT);
+
+var engines = require('consolidate');
+
+app.set('omnifood', __dirname + '/omnifood');
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/omnifood'));
-
+//app.use(express.static(path.join(__dirname, 'public')));
 // set the home page route
 app.get('/', function(req, res) {
 
