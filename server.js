99fecb42383ab95b4f53ef5e9c958eb9f367bd47
Ashok Tulachan <tulachanashok@Ashoks-MBP.fios-router.home>
2017-02-20 15:34:18 -0500

    Adding view engine

--- server.js
+++ server.js
@@ -3,9 +3,20 @@ const express = require('express');
 var app = express();
 const port = process.env.PORT || 3000;
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+console.log("Process PORT is: " + process.env.PORT);
+// make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/omnifood'));
-app.listen(port, () => {
-  console.log(`Started on port ${port}`);
+
+// set the home page route
+app.get('/', function(req, res) {
+
+	// ejs render automatically looks in the views folder
+	res.render('index');
 });
 
-module.exports = {app};
+app.listen(port, function() {
+	console.log('Our app is running on http://localhost:' + port);
+});
